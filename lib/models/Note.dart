be196cd922ee568ac67eaa0d9b72731a8545dfb6
Flath <Flath@DESKTOP-916916I>
2021-03-06 14:56:30 +0100

    update, modification of model to add id and isOdd function

--- lib/models/Note.dart
+++ lib/models/Note.dart
@@ -1,6 +1,12 @@
 class Note {
-  Note(this.title, this.description);
-
+  Note(this.id, this.title, this.content);
+  final int id;
   final String title;
-  final String description;
+  final String content;
+
+  get isOdd => this.id % 2 == 1;
+
+  getId() {
+    return this.id;
+  }
 }
