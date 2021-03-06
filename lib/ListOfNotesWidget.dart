a6637f7b66c92c9b4b99d9a0c1729adbdaf2624a
Flath <Flath@DESKTOP-916916I>
2021-03-06 11:14:22 +0100

    update, refactoring of list of notes and main file to make project more readable

---
+++ lib/ListOfNotesWidget.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// This is the stateful widget that the main application instantiates.
+class ListOfNotesWidget extends StatefulWidget {
+  ListOfNotesWidget({Key key}) : super(key: key);
+
+  @override
+  _ListOfNotesWidgetState createState() => _ListOfNotesWidgetState();
+}
+
+/// This is the private State class that goes with ListOfNotesWidget.
+class _ListOfNotesWidgetState extends State<ListOfNotesWidget> {
+  final List<int> _items = List<int>.generate(50, (int index) => index);
+
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final evenItemColor = colorScheme.primary.withOpacity(0.15);
+
+    return ReorderableListView(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      children: <Widget>[
+        for (int index = 0; index < _items.length; index++)
+          Card(
+            key: Key('$index'),
+            child: ListTile(
+              tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+              onTap: () => GestureTapCallback,
+              leading: Icon(Icons.notes_rounded),
+              trailing: Icon(Icons.delete),
+              title: Text('Item ${_items[index]}'),
+              subtitle: Text('Item description ${_items[index]}'),
+            ),
+          ),
+      ],
+      onReorder: (int oldIndex, int newIndex) {
+        setState(() {
+          if (oldIndex < newIndex) {
+            newIndex -= 1;
+          }
+          final int item = _items.removeAt(oldIndex);
+          _items.insert(newIndex, item);
+        });
+      },
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'ListOfNotesWidget.dart';
+
 void main() => runApp(MyApp());
 
 /// This is the main application widget.
@@ -14,54 +16,8 @@ class MyApp extends StatelessWidget {
       title: _title,
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: MyStatefulWidget(),
+        body: ListOfNotesWidget(),
       ),
-    );
-  }
-}
-
-/// This is the stateful widget that the main application instantiates.
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  final List<int> _items = List<int>.generate(50, (int index) => index);
-
-  Widget build(BuildContext context) {
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final oddItemColor = colorScheme.primary.withOpacity(0.05);
-    final evenItemColor = colorScheme.primary.withOpacity(0.15);
-
-    return ReorderableListView(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
-      children: <Widget>[
-        for (int index = 0; index < _items.length; index++)
-          Card(
-            key: Key('$index'),
-            child: ListTile(
-              tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
-              onTap: () => GestureTapCallback,
-              leading: Icon(Icons.notes_rounded),
-              trailing: Icon(Icons.delete),
-              title: Text('Item ${_items[index]}'),
-              subtitle: Text('Item description ${_items[index]}'),
-            ),
-          ),
-      ],
-      onReorder: (int oldIndex, int newIndex) {
-        setState(() {
-          if (oldIndex < newIndex) {
-            newIndex -= 1;
-          }
-          final int item = _items.removeAt(oldIndex);
-          _items.insert(newIndex, item);
-        });
-      },
     );
   }
 }
