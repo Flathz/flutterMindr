7f01243075e0fa3855eb11c4d0957a54ec11dbe2
Flath <Flath@DESKTOP-916916I>
2021-03-06 14:57:26 +0100

    update, change of list of items to list of notes

--- lib/ListOfNotesWidget.dart
+++ lib/ListOfNotesWidget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'MOCK.dart';
+import 'models/Note.dart';
 
 /// This is the stateful widget that the main application instantiates.
 class ListOfNotesWidget extends StatefulWidget {
@@ -11,7 +13,7 @@ class ListOfNotesWidget extends StatefulWidget {
 
 /// This is the private State class that goes with ListOfNotesWidget.
 class _ListOfNotesWidgetState extends State<ListOfNotesWidget> {
-  final List<int> _items = List<int>.generate(50, (int index) => index);
+  final List<Note> _items = list;
 
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
@@ -39,7 +41,7 @@ class _ListOfNotesWidgetState extends State<ListOfNotesWidget> {
           if (oldIndex < newIndex) {
             newIndex -= 1;
           }
-          final int item = _items.removeAt(oldIndex);
+          final Note item = _items.removeAt(oldIndex);
           _items.insert(newIndex, item);
         });
       },
